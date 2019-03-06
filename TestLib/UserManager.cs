e86cc40b4d550ba640ee84befecb18d90056c70f
Dzmitriy <Dzmitriy_Kakhanouski@epam.com>
2019-03-06 11:35:25 +0300

    Initial commit

---
+++ TestLib/UserManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TestLib
+{
+  public class UserManager
+  {
+    private string _name;
+
+    public string Name {
+      get => _name;
+      set => _name = value;
+    }
+
+    public UserManager(string name)
+    {
+      _name = name;
+    }
+    
+    public string GetGreeting()
+    {
+      return DateTime.Now.ToShortDateString() + $" Hello, {_name}! ";
+    }
+  }
+}
